d6b24795f732572e8f1d2cda195a4182e4a2b9e2
Mehmet Deniz Birlikci <mdbirlikci@gmail.com>
2023-06-17 14:05:17 -0700

    New implementation with mp3 file playing

--- twilio/side_twilio.py
+++ twilio/side_twilio.py
@@ -2,18 +2,22 @@ from twilio.rest import Client
 from twilio.twiml.voice_response import VoiceResponse
 import time
 import openai
+
 # Set up Twilio client
-account_sid = os.getenv("SID")
-auth_token = os.getenv("TWILIO_AUTH")
-twilio_phone_number = "+18448925795"
-recipient_phone_number = "+15104672510"
+account_sid = 'YOUR_TWILIO_ACCOUNT_SID'
+auth_token = 'YOUR_TWILIO_AUTH_TOKEN'
+twilio_phone_number = 'YOUR_TWILIO_PHONE_NUMBER'
+recipient_phone_number = 'RECIPIENT_PHONE_NUMBER'
+
 client = Client(account_sid, auth_token)
+
 # Set up OpenAI
-openai.api_key = "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"
+openai.api_key = 'YOUR_OPENAI_API_KEY'
+
 # Function to generate response using OpenAI
 def generate_response(prompt):
     response = openai.Completion.create(
-        engine="text-davinci-003",
+        engine='text-davinci-003',
         prompt=prompt,
         max_tokens=50,
         temperature=0.7,
@@ -21,37 +25,60 @@ def generate_response(prompt):
         stop=None
     )
     return response.choices[0].text.strip()
+
 # Function to handle incoming call
 def handle_incoming_call():
     response = VoiceResponse()
     response.say("Hello, how can I assist you?")
-    response.record(max_length=30, action="/process_recording")
+    response.record(max_length=30, action='/process_recording')
     return str(response)
+
 # Function to process the recording and generate a response
 def process_recording(recording_url):
     # Convert recording to text using speech-to-text API or library
-    # Here, let’s assume we have a function called `convert_speech_to_text` for this purpose
+    # Here, let's assume we have a function called `convert_speech_to_text` for this purpose
     recipient_message = convert_speech_to_text(recording_url)
+
     # Generate a response using OpenAI
-    prompt = "Recipient said: + recipient_message"
+    prompt = "Recipient said: " + recipient_message
     generated_response = generate_response(prompt)
+
+    # Save the generated response as an audio file
+    audio_filename = 'generated_response.mp3'
+    save_generated_response_as_audio(generated_response, audio_filename)
+
     # Respond to the recipient with the generated answer
     response = VoiceResponse()
-    response.say(generated_response)
-    response.record(max_length=30, action=‘/process_recording’)
+    response.play(audio_filename)
+    response.record(max_length=30, action='/process_recording')
     return str(response)
+
+# Function to save the generated response as an audio file
+def save_generated_response_as_audio(generated_response, filename):
+    # Convert the generated response to audio and save it as the specified filename
+    # Here, you will need to use a suitable text-to-speech library or service to convert the text to audio
+    # Ensure that the audio file is saved with the .mp3 extension
+
+    # Example code using the pyttsx3 library
+    import pyttsx3
+    engine = pyttsx3.init()
+    engine.save_to_file(generated_response, filename)
+    engine.runAndWait()
+
 # Create a Twilio call
 call = client.calls.create(
     twiml=handle_incoming_call(),
     to=recipient_phone_number,
     from_=twilio_phone_number
 )
+
 # Twilio webhook to process the recording and generate a response
-@app.route(‘/process_recording’, methods=[‘POST’])
+@app.route('/process_recording', methods=['POST'])
 def process_recording_webhook():
-    recording_url = request.form[‘RecordingUrl’]
+    recording_url = request.form['RecordingUrl']
     response = process_recording(recording_url)
     return response
+
 # Start the Flask server to listen for incoming requests
-if __name__ == ‘__main__‘:
+if __name__ == '__main__':
     app.run()
