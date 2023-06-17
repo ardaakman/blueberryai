f8d9ebb9b13992ec87c592626c63bf32b589efd0
apaul30 <anshulpaul@berkeley.edu>
2023-06-17 12:11:29 -0700

    html files

--- API.py
+++ API.py
@@ -9,17 +9,42 @@ app = FastAPI()
 api_router = APIRouter()
 BASE_PATH = Path(__file__).resolve().parent
 app.mount("/static", StaticFiles(directory="static"), name="static")
-TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates/employees"))
+TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 
-@app.get("/")
-async def root(request: Request):
+@app.get("/", response_class=HTMLResponse)
+async def init(request: Request):
     '''
-    Init call page
+    Load page to set call settings
     '''
-    return {"message": "Hello World"}
-    # return TEMPLATES.TemplateResponse(
-    #     "init.html",
-    #     {
-    #         "request": request,
-    #     }
-    # )
+    return TEMPLATES.TemplateResponse(
+        "init.html",
+        {
+            "request": request,
+        }
+    )
+
+
+@app.get("/call/{call_id}", response_class=HTMLResponse)
+async def call(request: Request, call_id: str):
+    '''
+    Page to view ongoinng call
+    '''
+    return TEMPLATES.TemplateResponse(
+        "call.html",
+        {
+            "request": request,
+        }
+    )
+
+
+@app.get("/history", response_class=HTMLResponse)
+async def history(request: Request):
+    '''
+    Page to view call history
+    '''
+    return TEMPLATES.TemplateResponse(
+        "history.html",
+        {
+            "request": request,
+        }
+    )
